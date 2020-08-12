11aa9c3c424e0a730fc975e43131c99359b57355
zhenyasmagin@yandex.ru <zhenyasmagin@yandex.ru>
2020-08-12 17:17:22 +0300

    add console output of simple response

--- src/main/java/com/smagin/smartweatherservice/WeatherController.java
+++ src/main/java/com/smagin/smartweatherservice/WeatherController.java
@@ -1,20 +1,39 @@
 package com.smagin.smartweatherservice;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
+import org.springframework.web.client.RestTemplate;
 
 @RequiredArgsConstructor
 @RestController
 public class WeatherController {
-    @GetMapping("/{weather/{city}/{date}")
-    public WeatherResponse weatherByCityOnDate(@PathVariable("city") String city,
-                                               @PathVariable("date")
-                                               @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)  LocalDate localDate){
-       return null;
+    @Autowired
+    private RestTemplate restTemplate;
+    @GetMapping("/weather/{city}")
+    public WeatherResponse weatherByCityOnDate(@PathVariable("city") Integer city
+                                               /*@PathVariable("date")
+                                               @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)  LocalDate localDate*/){
+        ResponseEntity<String> forEntity = restTemplate.getForEntity("http://api.openweathermap.org/data/2.5/weather?id="
+                + city + "&appid=1d17116b3ac212464b3dfc2f189f1aa5", String.class);
+        System.out.println(forEntity.getBody());
+        JSONObject jsonObject  = new JSONObject(forEntity.getBody());
+        jsonObject.get("base");
+        System.out.println("!!!!!!!!!"+ jsonObject.get("base"));
+        return null;
+    }
+
+    @Configuration
+    public static class WebConfig{
+        @Bean
+        public RestTemplate restTemplate(){
+            return  new RestTemplate();
+        }
     }
 }
